a025d3596097049a7dca098b945c149f39bf6115
01weal1mst@hft-stuttgart.de <01weal1mst@hft-stuttgart.de@4069bb5b-c0ed-cd3a-4ab6-0ff420d54452>
2010-06-03 15:24:49 +0000

    More initial stuff

---
+++ CarRental/src/de/hft/carrental/ui/main/bookings/BookingsTableSection.java
@@ -0,0 +1,9 @@
+package de.hft.carrental.ui.main.bookings;
+
+import de.hft.carrental.ui.main.TableSection;
+
+public final class BookingsTableSection extends TableSection {
+
+	private static final long serialVersionUID = 6099129396844699288L;
+
+}
