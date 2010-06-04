63bea28baf9a355f199225d91c102a9786860871
matthias.ruszala <matthias.ruszala@4069bb5b-c0ed-cd3a-4ab6-0ff420d54452>
2010-06-04 14:52:38 +0000

    -added name length to Agency -added fields/methods for AGENCY_ID forgein key to Branch -annotated Branch

--- CarRental/src/de/hft/carrental/domain/Branch.java
+++ CarRental/src/de/hft/carrental/domain/Branch.java
@@ -1,17 +1,39 @@
 package de.hft.carrental.domain;
 
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "BRANCH")
 public final class Branch {
 
 	private Integer id;
 
 	private String name;
 
+	private Integer agencyId;
+
+	@Id
+	@GeneratedValue
+	@Column(name = "ID", updatable = false, nullable = false)
 	public Integer getId() {
 		return id;
 	}
 
+	@Column(name = "NAME", updatable = true, nullable = false, length = 45)
 	public String getName() {
 		return name;
+	}
+
+	@ManyToOne(cascade = CascadeType.ALL, optional = false)
+	@Column(name = "AGENCY_ID", updatable = true, nullable = false)
+	public Integer getAgencyId() {
+		return agencyId;
 	}
 
 	public void setId(Integer id) {
@@ -20,6 +42,10 @@ public final class Branch {
 
 	public void setName(String name) {
 		this.name = name;
+	}
+
+	public void setAgencyId(Integer agencyId) {
+		this.agencyId = agencyId;
 	}
 
 }
