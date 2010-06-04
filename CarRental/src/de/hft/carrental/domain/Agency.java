45f0c74a11d85d285c98b831502ca07627c64f1d
matthias.ruszala <matthias.ruszala@4069bb5b-c0ed-cd3a-4ab6-0ff420d54452>
2010-06-04 14:16:43 +0000

    Annotated Agency

--- CarRental/src/de/hft/carrental/domain/Agency.java
+++ CarRental/src/de/hft/carrental/domain/Agency.java
@@ -1,15 +1,27 @@
 package de.hft.carrental.domain;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "AGENCY")
 public final class Agency {
 
 	private Integer id;
 
 	private String name;
 
+	@Id
+	@GeneratedValue
+	@Column(name = "ID", updatable = false, nullable = false)
 	public Integer getId() {
 		return id;
 	}
 
+	@Column(name = "NAME", updatable = true, nullable = false)
 	public String getName() {
 		return name;
 	}
