4177f54bb5c494ad5831a87e08ce49ab7f4232f8
01weal1mst <01weal1mst@hft-stuttgart.de>
2010-06-05 14:11:50 +0000

    Even more documentation and clean up

--- CarRental/src/de/hft/carrental/ui/splash/login/LoginPage.java
+++ CarRental/src/de/hft/carrental/ui/splash/login/LoginPage.java
@@ -1,14 +1,23 @@
 package de.hft.carrental.ui.splash.login;
 
-import de.hft.carrental.ui.Window;
 import de.hft.carrental.ui.WindowPage;
+import de.hft.carrental.ui.splash.SplashWindow;
 
+/**
+ * This page is used by the {@link SplashWindow} to enable the user to login.
+ * 
+ * @author Alexander Weickmann
+ */
 public final class LoginPage extends WindowPage {
 
 	private static final long serialVersionUID = -2590619285921957633L;
 
-	public LoginPage(Window window) {
-		super(window, 1, 1);
+	/**
+	 * @param splashWindow
+	 *            The {@link SplashWindow} this page belongs to.
+	 */
+	public LoginPage(SplashWindow splashWindow) {
+		super(splashWindow, 1, 1);
 	}
 
 	@Override
