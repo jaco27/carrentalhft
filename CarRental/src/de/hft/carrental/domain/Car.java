66433ec9428e2f9439288f4709b231346534d43d
matthias.ruszala <matthias.ruszala@4069bb5b-c0ed-cd3a-4ab6-0ff420d54452>
2010-06-05 11:31:24 +0000

    -started annotating Car

--- CarRental/src/de/hft/carrental/domain/Car.java
+++ CarRental/src/de/hft/carrental/domain/Car.java
@@ -2,6 +2,14 @@ package de.hft.carrental.domain;
 
 import java.util.Date;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "CAR")
 public final class Car {
 
 	private Float basePricePerDay;
@@ -14,22 +22,29 @@ public final class Car {
 
 	private String registrationNumber;
 
+	@Column(name = "BASE_PRICE_PER_DAY", updatable = true, nullable = false)
 	public Float getBasePricePerDay() {
 		return basePricePerDay;
 	}
 
+	@Column(name = "COLOR", updatable = true, nullable = false, length = 30)
 	public String getColor() {
 		return color;
 	}
 
+	@Column(name = "DATE_OF_MANUFACTURING", updatable = false, nullable = false)
 	public Date getDateOfManufacturing() {
 		return dateOfManufacturing;
 	}
 
+	@Id
+	@GeneratedValue
+	@Column(name = "ID", updatable = false, nullable = false)
 	public Integer getId() {
 		return id;
 	}
 
+	@Column(name = "REGISTRATION_NUMBER", updatable = false, nullable = false)
 	public String getRegistrationNumber() {
 		return registrationNumber;
 	}
