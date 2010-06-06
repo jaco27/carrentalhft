95c9c755124c0ec7c191309f77efe8b17a01362f
matthias.ruszala <matthias.ruszala@4069bb5b-c0ed-cd3a-4ab6-0ff420d54452>
2010-06-06 16:32:22 +0000

    -started work on general info section

--- CarRental/src/de/hft/carrental/ui/main/personal/GeneralInfoSection.java
+++ CarRental/src/de/hft/carrental/ui/main/personal/GeneralInfoSection.java
@@ -1,5 +1,9 @@
 package de.hft.carrental.ui.main.personal;
 
+import javax.swing.JLabel;
+import javax.swing.JTextField;
+
+import net.miginfocom.swing.MigLayout;
 import de.hft.carrental.ui.main.MainWindowPage;
 import de.hft.carrental.ui.main.MainWindowPageSection;
 
@@ -8,8 +12,33 @@ public final class GeneralInfoSection extends MainWindowPageSection {
 
 	private static final long serialVersionUID = 2921841683848149881L;
 
+	private JLabel loginLabel = new JLabel("Login name:");
+	private JLabel registerDateLabel = new JLabel("Register date:");
+	private JLabel emailLabel = new JLabel("Email:");
+	private JLabel firstNameLabel = new JLabel("First name:");
+	private JLabel lastNameLabel = new JLabel("Last name:");
+	private JLabel surNameLabel = new JLabel("Surname:");
+	private JLabel birthDateLabel = new JLabel("Date of birth:");
+	private JLabel companyNameLabel = new JLabel("Company name: ");
+
+	private JTextField loginField = new JTextField();
+	private JTextField registerField = new JTextField();
+	private JTextField emailField = new JTextField();
+	private JTextField firstNameField = new JTextField();
+	private JTextField flastNameField = new JTextField();
+	private JTextField surNameField = new JTextField();
+	private JTextField birthDateField = new JTextField();
+	private JTextField companyNameField = new JTextField();
+
 	protected GeneralInfoSection(MainWindowPage page) {
-		super(page, "INSERT TITLE");
+		super(page, "Personal details");
+		setLayout(new MigLayout());
+		createContents();
+	}
+
+	private void createContents() {
+		add(loginLabel);
+		add(loginField, "wrap");
 	}
 
 	@Override
