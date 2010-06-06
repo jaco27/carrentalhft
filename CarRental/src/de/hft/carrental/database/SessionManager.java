e87e57939f4910a8e8d666d5d3aa685c91baf7b9
01weal1mst <01weal1mst@hft-stuttgart.de>
2010-06-06 17:26:21 +0000

    Annoation mapping is now configured by it's external config file

--- CarRental/src/de/hft/carrental/database/SessionManager.java
+++ CarRental/src/de/hft/carrental/database/SessionManager.java
@@ -8,15 +8,6 @@ import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.AnnotationConfiguration;
 
-import de.hft.carrental.domain.Agency;
-import de.hft.carrental.domain.Booking;
-import de.hft.carrental.domain.Branch;
-import de.hft.carrental.domain.BranchAddress;
-import de.hft.carrental.domain.Car;
-import de.hft.carrental.domain.CarType;
-import de.hft.carrental.domain.Customer;
-import de.hft.carrental.domain.CustomerAddress;
-
 /**
  * The session manager is a singleton that enables clients to retrieve sessions
  * from everywhere in the code.
@@ -27,24 +18,15 @@ public final class SessionManager {
 
 	private static final SessionManager instance = new SessionManager();
 
-	private AnnotationConfiguration configuration;
+	private final AnnotationConfiguration configuration;
 
 	private final SessionFactory sessionFactory;
 
 	private Session session;
 
 	private SessionManager() {
-		// TODO Read annotation configuration from external configuration file.
-		configuration = new AnnotationConfiguration();
-		configuration = configuration.addAnnotatedClass(Agency.class);
-		configuration = configuration.addAnnotatedClass(Booking.class);
-		configuration = configuration.addAnnotatedClass(Branch.class);
-		configuration = configuration.addAnnotatedClass(BranchAddress.class);
-		configuration = configuration.addAnnotatedClass(Car.class);
-		configuration = configuration.addAnnotatedClass(CarType.class);
-		configuration = configuration.addAnnotatedClass(Customer.class);
-		configuration = configuration.addAnnotatedClass(CustomerAddress.class);
-
+		configuration = new AnnotationConfiguration()
+				.configure("hibernate.cfg.xml");
 		sessionFactory = configuration.buildSessionFactory();
 		openSession();
 	}
