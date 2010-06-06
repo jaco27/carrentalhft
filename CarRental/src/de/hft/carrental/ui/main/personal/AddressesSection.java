7ea887e53670af14041520b4f5009b82895ea369
matthias.ruszala <matthias.ruszala@4069bb5b-c0ed-cd3a-4ab6-0ff420d54452>
2010-06-06 21:16:59 +0000

    -Initial implementation of CRUD dialog

--- CarRental/src/de/hft/carrental/ui/main/personal/AddressesSection.java
+++ CarRental/src/de/hft/carrental/ui/main/personal/AddressesSection.java
@@ -1,5 +1,15 @@
 package de.hft.carrental.ui.main.personal;
 
+import java.awt.Toolkit;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JButton;
+import javax.swing.JDialog;
+import javax.swing.JLabel;
+import javax.swing.JTextField;
+
+import net.miginfocom.swing.MigLayout;
 import de.hft.carrental.domain.CustomerAddress;
 import de.hft.carrental.ui.main.MainWindowPage;
 import de.hft.carrental.ui.main.TableSection;
@@ -19,6 +29,16 @@ public final class AddressesSection extends TableSection {
 
 	protected AddressesSection(MainWindowPage page) {
 		super(page, "Address(es)", COLUMN_NAMES, COLUMN_WIDTHS);
+
+		JButton edit = new JButton("Edit");
+		edit.addActionListener(new ActionListener() {
+
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				new AddressDialog().setVisible(true);
+			}
+		});
+		add(edit);
 	}
 
 	@Override
@@ -43,5 +63,85 @@ public final class AddressesSection extends TableSection {
 
 	private void saveChanges() {
 		// TODO implement
+	}
+}
+
+class AddressDialog extends JDialog {
+
+	private static final long serialVersionUID = 1L;
+
+	private final Object[] content = new Object[6];
+
+	private JLabel streetLabel = new JLabel("Street:");
+	private JLabel numberLabel = new JLabel("Street Number:");
+	private JLabel postalLabel = new JLabel("Postal code:");
+	private JLabel cityLabel = new JLabel("City:");
+	private JLabel countryLabel = new JLabel("Country:");
+	private JLabel phoneLabel = new JLabel("Phone number");
+
+	private JTextField streetField = new JTextField(45);
+	private JTextField numberField = new JTextField(5);
+	private JTextField postalField = new JTextField(10);
+	private JTextField cityField = new JTextField(45);
+	private JTextField countryField = new JTextField(45);
+	private JTextField phoneField = new JTextField(45);
+
+	private JButton cancel = new JButton("Cancel");
+	private JButton previous = new JButton("Previous");
+	private JButton next = new JButton("Next");
+	private JButton save = new JButton("Save");
+
+	public AddressDialog() {
+		super();
+
+		setModalityType(ModalityType.APPLICATION_MODAL);
+
+		buildLayout();
+		addButtonActions();
+
+		pack();
+		int posX = Toolkit.getDefaultToolkit().getScreenSize().width / 2
+				- getWidth() / 2;
+		int posY = Toolkit.getDefaultToolkit().getScreenSize().height / 2
+				- getHeight() / 2;
+		setLocation(posX, posY);
+	}
+
+	private void buildLayout() {
+		setLayout(new MigLayout("", "[][grow]"));
+
+		add(streetLabel);
+		add(streetField, "growx, wrap");
+
+		add(numberLabel);
+		add(numberField, "growx, wrap");
+
+		add(postalLabel);
+		add(postalField, "growx, wrap");
+
+		add(cityLabel);
+		add(cityField, "growx, wrap");
+
+		add(countryLabel);
+		add(countryField, "growx, wrap");
+
+		add(phoneLabel);
+		add(phoneField, "growx, wrap");
+
+		add(previous, "split 2, align left");
+		add(next);
+		add(save, "split 2, align right");
+		add(cancel);
+
+	}
+
+	private void addButtonActions() {
+		cancel.addActionListener(new ActionListener() {
+
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				setVisible(false);
+			}
+		});
 	}
 }
