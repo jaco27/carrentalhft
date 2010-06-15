43723fa7f9b34f8ecd8a608c44099ab89eb2ce78
01weal1mst@hft-stuttgart.de <01weal1mst@hft-stuttgart.de@4069bb5b-c0ed-cd3a-4ab6-0ff420d54452>
2010-06-15 20:29:28 +0000

    had to out-comment new code cause of compile errors

--- CarRental/src/de/hft/carrental/ui/main/cars/SearchCarsSection.java
+++ CarRental/src/de/hft/carrental/ui/main/cars/SearchCarsSection.java
@@ -1,58 +1,55 @@
 package de.hft.carrental.ui.main.cars;
 
-import de.hft.carrental.ui.main.MainWindowPage;
+import javax.swing.JButton;
+import javax.swing.JLabel;
+import javax.swing.JTextField;
+
 import de.hft.carrental.ui.main.MainWindowPageSection;
-import java.util.Date;
 
 //TODO RM, PS: Class yet to be implemented.
 public final class SearchCarsSection extends MainWindowPageSection {
 
 	private static final long serialVersionUID = -860724473744347648L;
-	
- //Labels for search page     
-    private final JLabel CarName = new JLabel("Car Name:");
-    private final JLabel Pickup = new JLabel("Pick up Location:");
-    private final JLabel FromDate = new JLabel("From:");
-    private final JLabel ToDate = new JLabel("To:");
- 
-  //Textfields for search page 
-    private final JTextField CarNameField = new JTextField();
-    private final JTextField PickupField = new JTextField();
-    private final JTextField FromDateField = new JTextField();
-    private final JTextField ToDateField = new JTextField();
-    
-  //Button for search   
-    private final JButton searchButton = new JButton("Search");
 
-    protected SearchCarsSection(SearchCarsSection CarsPage) {
-        super(page, "Search Cars");
-    }
+	// Labels for search page
+	private final JLabel CarName = new JLabel("Car Name:");
+	private final JLabel Pickup = new JLabel("Pick up Location:");
+	private final JLabel FromDate = new JLabel("From:");
+	private final JLabel ToDate = new JLabel("To:");
 
+	// Textfields for search page
+	private final JTextField CarNameField = new JTextField();
+	private final JTextField PickupField = new JTextField();
+	private final JTextField FromDateField = new JTextField();
+	private final JTextField ToDateField = new JTextField();
+
+	// Button for search
+	private final JButton searchButton = new JButton("Search");
+
+	protected SearchCarsSection(CarsPage carsPage) {
+		super(carsPage, "Search Cars");
+	}
 
 	@Override
 	protected void refresh() {
-		CarNameField.setText(user.getCarName());
-        PickupField.setText(user.getPickup());
-        FromDateField.setText(user.getFromDate());
-        ToDateField.setText(user.getToDate());
-      
-
-        DateFormat df = DateFormat.getInstance();
-        FromDateField.setText(df.format(user.getFromDate()));
-        ToDateField.setText(df.format(user.getToDate()));
-        searchButton.setEnabled(false);
+		// CarNameField.setText(user.getCarName());
+		// PickupField.setText(user.getPickup());
+		// FromDateField.setText(user.getFromDate());
+		// ToDateField.setText(user.getToDate());
+		//
+		// DateFormat df = DateFormat.getInstance();
+		// FromDateField.setText(df.format(user.getFromDate()));
+		// ToDateField.setText(df.format(user.getToDate()));
+		// searchButton.setEnabled(false);
 	}
-	
+
 	private void addListeners() {
-        searchButton.addActionListener(this);        
-    }
-    
-    @Override
-    public void actionPerformed(ActionEvent e) {
-                    searchButton.setEnabled(True);
-                    CarsTableSection();
-    }
+		// searchButton.addActionListener(this);
+	}
+
+	// @Override
+	// public void actionPerformed(ActionEvent e) {
+	// searchButton.setEnabled(True);
+	// CarsTableSection();
+	// }
 }
-
-
-
