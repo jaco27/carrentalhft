e0d513b10649f7b9df1e65f62d4d920c941c9bd2
priya4484 <priya4484@4069bb5b-c0ed-cd3a-4ab6-0ff420d54452>
2010-06-15 20:19:51 +0000

    This section contains the code for the search page with textfields, buttons and labels for needed search criteria.

--- CarRental/src/de/hft/carrental/ui/main/cars/SearchCarsSection.java
+++ CarRental/src/de/hft/carrental/ui/main/cars/SearchCarsSection.java
@@ -2,19 +2,57 @@ package de.hft.carrental.ui.main.cars;
 
 import de.hft.carrental.ui.main.MainWindowPage;
 import de.hft.carrental.ui.main.MainWindowPageSection;
+import java.util.Date;
 
 //TODO RM, PS: Class yet to be implemented.
 public final class SearchCarsSection extends MainWindowPageSection {
 
 	private static final long serialVersionUID = -860724473744347648L;
+	
+ //Labels for search page     
+    private final JLabel CarName = new JLabel("Car Name:");
+    private final JLabel Pickup = new JLabel("Pick up Location:");
+    private final JLabel FromDate = new JLabel("From:");
+    private final JLabel ToDate = new JLabel("To:");
+ 
+  //Textfields for search page 
+    private final JTextField CarNameField = new JTextField();
+    private final JTextField PickupField = new JTextField();
+    private final JTextField FromDateField = new JTextField();
+    private final JTextField ToDateField = new JTextField();
+    
+  //Button for search   
+    private final JButton searchButton = new JButton("Search");
 
-	protected SearchCarsSection(MainWindowPage page) {
-		super(page, "INSERT TITLE");
-	}
+    protected SearchCarsSection(SearchCarsSection CarsPage) {
+        super(page, "Search Cars");
+    }
+
 
 	@Override
 	protected void refresh() {
-		// TODO RM, PS: Method yet to be implemented.
-	}
+		CarNameField.setText(user.getCarName());
+        PickupField.setText(user.getPickup());
+        FromDateField.setText(user.getFromDate());
+        ToDateField.setText(user.getToDate());
+      
 
+        DateFormat df = DateFormat.getInstance();
+        FromDateField.setText(df.format(user.getFromDate()));
+        ToDateField.setText(df.format(user.getToDate()));
+        searchButton.setEnabled(false);
+	}
+	
+	private void addListeners() {
+        searchButton.addActionListener(this);        
+    }
+    
+    @Override
+    public void actionPerformed(ActionEvent e) {
+                    searchButton.setEnabled(True);
+                    CarsTableSection();
+    }
 }
+
+
+
